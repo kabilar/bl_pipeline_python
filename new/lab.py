9216d89e961711bdfc955b7083b034ddb1c9aa12
Kabilar Gunalan <kabilar@vathes.com>
2020-11-21 00:19:56 -0600

    Transfer lab and subject schemas to new tables - work in progress

---
+++ new/lab.py
@@ -0,0 +1,84 @@
+import datajoint as dj
+
+lab = dj.schema('bl_new_lab')
+
+@lab
+class Contacts(dj.Manual):
+    definition = """
+    user_id                    : varchar(32)      # PNI netID or similar
+    -----
+    contacts_old_id            : int(11)
+    experimenter               : varchar(32)      # first name (must be unique so may need to add last initial)
+    email=null                 : varchar(150)     # princeton email address
+    initials=null              : varchar(5)       # 2 letter initials
+    telephone=null             : bigint(20)       # cell phone digits only
+    tag_letter=null            : varchar(1)       # first letter of rat's name
+    lab_manager=0              : tinyint(1)       # 1 if you're lab manager
+    subscribe_all=0            : tinyint(1)       # 1 is you want to get all automated emails
+    tech_morning=0             : tinyint(1)       # 1 if you're a B shift tech
+    tech_afternoon=0           : tinyint(1)       # 1 if you're a C shift tech
+    tech_computer=0            : tinyint(1)       # 1 if you're the computer tech
+    is_alumni=0                : tinyint(1)       # 1 if you're an alumni of the lab
+    full_name=null             : varchar(60)      # You're full name however you like it
+    tech_overnight=0           : tinyint(1)       # 1 if you're the A shift tech
+    tag_rgb=null               : char(12)         # 3 integers 0 to 255 determines the color of your cage cards
+    tech_shifts                : varchar(45)      # shifts a tech is scheduled to work, rigs an experimenter is responsible for fixing
+    phone_carrier=null         : varchar(15)      # cell phone network provider, necessary to send text messages
+    """
+
+@lab
+class Riginfo(dj.Manual):
+    definition = """
+    rigid                      : int(3)           # Unique rig integer number
+    -----
+    rigname                    : varchar(32)      # THIS CURRENTLY DOES NOT EXIST 
+    ip_addr=''                 : char(15)         # IP address of the rig
+    mac_addr=''                : char(12)         # MAC address of the rig
+    rtfsm_ip=null              : char(15)         # The Linux machine this rig connects to
+    hostname=null              : varchar(50)      # URL for the rig as registered with OIT, i.e. brodyrigwt06.princeton.edu
+    comptype=''                : char(50)         # Processor and Operating System info
+    """
+
+@lab
+class Rigflush(dj.Manual):
+    definition = """
+    -> Riginfo
+    rigflush_date              : datetime
+    -----
+    rigflush_old_id            : int(10)
+    wasflushed                 : tinyint(3)
+    """
+
+@lab
+class Rigfood(dj.Manual):
+    definition = """
+    rigfood_id                 : int(10)
+    -----
+    -> Riginfo
+    rigfood_datetime           : datetime
+    """
+
+@lab
+class RigMaintenance(dj.Manual):
+    definition = """
+    rig_maintenance_id         : int(10)
+    -----
+    -> Riginfo
+    rig_fix_date               : datetime         # date and time the rig was fixed
+    rig_maintenance_note       : varchar(500)     # description of what is wrong with a rig 
+    isbroken                   : tinyint(3)       # 1 if the rig is still broken
+    broke_person               : varchar(45)      # lab member that flagged the rig as broken
+    fix_person                 : varchar(45)      # lab member that fixed the rig
+    broke_date                 : datetime         # date and time when the rig was flagged as broken
+    rig_maintenance_fix_note   : varchar(500)     # description of what was done to fix the rig
+    """
+
+@lab
+class TrainingRoom(dj.Manual):
+    definition = """
+    tower                      : int(10)          # unique ID, the tower the rig belongs to
+    -----
+    top=null                   : char(50)         # top    rig in tower: ratname, trial count, percent correct, training duration
+    middle=null                : char(50)         # middle rig in tower: ratname, trial count, percent correct, training duration
+    bottom=null                : char(50)         # bottom rig in tower: ratname, trial count, percent correct, training duration
+    """
---
+++ new/subject.py
@@ -0,0 +1,52 @@
+import datajoint as dj
+from new import lab
+
+subject = dj.schema('bl_new_subject')
+
+@subject
+class Rats(dj.Manual):
+    definition = """
+    ratname                     : varchar(8)      # Unique rat name, 1 letter 3 numbers
+    -----
+    -> lab.Contacts
+    rats_old_id                 : int(10)         # was a unique number assigned to each rat
+    free=0                      : tinyint(1)      # whether or not the rat is available for use by any other experimenter
+    contact=null                : varchar(40)     # the PUIDs of the lab member(s) responsible for the rat
+    comments=null               : varchar(500)    # any notes about the animal, surgery info, transgenic strain...
+    vendor='Taconic'            : varchar(40)     # e.g. Taconic, Charles River, etc.
+    waterperday=30              : decimal(3,1)    # WHERE IT IS THE PERCENT BODY MASS THE RAT WILL BE OFFERED IN THE PUB, VALUES < 3.0 ARE NOT ALLOWED BY PROTOCOL, 100 INTERPRETED AS TO BE EXCLUDED FROM THE PUB AND RECEIVE WATER IN HOME CAGE
+    recovering=0                : tinyint(1)      # whether or not the rat is currently in recovery from surgery or illness
+    deliverydate=null           : date            # date rat arrived at lab
+    extant=0                    : tinyint(4)      # 1 if the rat is still alive
+    cagemate=null               : varchar(4)      # the name of the rat this rat lives with SHOULD BE ENLARGED TO ALLOW FOR MICE THAT HAVE MORE THAN 1 CAGEMATE
+    force_free_water=0          : tinyint(3)      # not used
+    date_sac=null               : date            # date the rat was euthanized or died
+    force_dep_water=0           : tinyint(3)      # training session during which a rat should receive water, for restricting rats not on the training schedule
+    larid=null                  : int(10)         # the ID assigned to the rat by LAR (I don't think we use this)
+    israt=1                     : tinyint(1)      # 1 if rat, 0 if mouse
+    """
+
+@subject
+class RatHistory(dj.Manual):
+    definition = """
+    -> Rats
+    logtime=CURRENT_TIMESTAMP   : timestamp       # IF THIS IS A HISTORICAL RECORD OF THE RATS TABLE THEN IT SHOULD HAVE ALL THE SAME COLUMNS
+    -----
+    -> lab.Contacts
+    rathistory_old_id           : int(10)
+    free=0                      : tinyint(1)      # 'whether or not the rat is available for use by any other experimenter',
+    alert=0                     : tinyint(1)      # 'whether or not there is an alert for special attention for the rat (e.g. sick, recent surgery, etc.)',
+    contact=null                : varchar(40)   
+    training=0                  : tinyint(1)   
+    comments=null               : varchar(500)   
+    waterperday=30              : int(11)         # '0 for free water, otherwise the number of minutes the rat has access to water each day (may vary over time)',
+    recovering=0                : tinyint(1)      # 'whether or not the rat is currently in recovery from surgery',
+    extant=0                    : tinyint(4)   
+    cagemate=null               : varchar(4)   
+    force_free_water=0          : tinyint(3) 
+    date_sac=null               : date   
+    force_dep_water=0           : tinyint(3) 
+    bring_up_at=0               : tinyint(1)      # 'set with which rat should be brought upstairs',
+    bringupday=null             : varchar(7)   
+    ignored_by_watermeister=0   : tinyint(1)
+    """
