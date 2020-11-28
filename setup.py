3670f385c4d1ba7538c49f880eead3a501686a09
shenshan <shenshanpku@gmail.com>
2020-11-27 21:03:22 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='brody_lab_pipeline',
+    version='0.0.0',
+    description='DataJoint pipeline for Carlos Bordy Lab',
+    author='Vathes',
+    author_email='support@vathes.com',
+    packages=find_packages(exclude=[]),
+    install_requires=['datajoint>=0.12', 'dash', 'dash-bootstrap-components'],
+)
