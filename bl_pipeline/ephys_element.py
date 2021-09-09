63b49d74863ff8731e5c52024e4b7b8a1fa48053
Kabilar Gunalan <kabilar@vathes.com>
2021-09-09 16:12:03 -0400

    Fix handling of multiple root data directories

--- bl_pipeline/ephys_element.py
+++ bl_pipeline/ephys_element.py
@@ -52,10 +52,8 @@ class SkullReference(dj.Lookup):
 
 def get_ephys_root_data_dir():
     data_dir = dj.config.get('custom', {}).get('ephys_root_data_dir', None)
-    return pathlib.Path(data_dir) if data_dir else None
 
-
-
+    return [pathlib.Path(data_dir) for data_dir in dj.config.get('custom', {}).get('ephys_root_data_dir', None)]
 
 def get_session_directory(session_key):
     root_dir = get_ephys_root_data_dir()
