3233b85f474e1b9adaea26b30ebf5e66d54c6407
Kabilar Gunalan <kabilar@vathes.com>
2021-10-01 14:38:12 -0400

    Fix path functions and refactor script

--- bl_pipeline/ephys_element.py
+++ bl_pipeline/ephys_element.py
@@ -1,39 +1,39 @@
-from os import PathLike
-import datajoint as dj
-import pathlib
-
-from element_data_loader import path_utils
-from element_array_ephys import probe as probe_element
-from element_array_ephys import ephys_chronic as ephys_element
-
 """
------- Gathering requirements to activate the ephys elements ------
-To activate the ephys elements, we need to provide:
+Requirements to activate the ephys elements.
 
 1. Schema names
-    + schema name for the probe module
-    + schema name for the ephys module
+    + Schema name for the probe module
+    + Schema name for the ephys module
 
 2. Upstream tables
     + Subject table
     + Session table
-    + SkullReference table - Reference table for InsertionLocation, specifying the skull reference
-                 used for probe insertion location (e.g. Bregma, Lambda)
+    + SkullReference table - Reference table for InsertionLocation, 
+                             specifying the skull reference used for probe 
+                             insertion location (e.g. Bregma, Lambda)
 
-3. Utility functions
+3. Path functions
     + get_ephys_root_data_dir()
     + get_session_directory()
 
-For more detail, check the docstring of the imaging element:
+4. Activate element
+
+For more detail, check the docstring of the element:
     help(probe_element.activate)
     help(ephys_element.activate)
 """
 
-# 1. Schema names
+# 1. Schema names --------------------------------------------------------------
+import datajoint as dj
+
+from element_array_ephys import probe as probe_element
+from element_array_ephys import ephys_chronic as ephys_element
+
 probe_schema_name = dj.config['custom']['database.prefix'] + 'probe_element'
 ephys_schema_name = dj.config['custom']['database.prefix'] + 'ephys_element'
 
-# 2. Upstream tables
+
+# 2. Upstream tables -----------------------------------------------------------
 from bl_pipeline import lab, subject, acquisition
 from bl_pipeline.acquisition import AcquisitionSessions as Session
 from bl_pipeline.subject import Rats as Subject
@@ -49,37 +49,25 @@ class SkullReference(dj.Lookup):
     contents = zip(['Bregma', 'Lambda'])
 
 
-# 3. Utility functions
-
+# 3. Path functions ------------------------------------------------------------
 def get_ephys_root_data_dir():
-    root_data_dirs = dj.config.get('custom', {}).get('ephys_root_data_dir', None)
+    root_data_dirs = dj.config.get('custom', {}).get('ephys_root_data_dir', 
+                                                     None)
 
-    if len(root_data_dirs) == 1:
-        return pathlib.Path(root_data_dirs)
-    else:
-        return [pathlib.Path(root_dir) for root_dir in root_data_dirs]
+    return root_data_dirs
 
 def get_session_directory(session_key): 
-    root_dirs = get_ephys_root_data_dir()
-    relative_dir = (Session & session_key).fetch1('acquisition_raw_rel_path')
-    session_dir = path_utils.find_full_path(root_dirs, relative_dir)
+    session_dir = (Session & session_key).fetch1('acquisition_raw_rel_path')
 
-    return session_dir.as_posix()
-
-    '''
-    experimenter, ratname, session_date = \
-        (lab.Contacts *
-         (subject.Rats *
-          (acquisition.Sessions.proj('session_date', ratname='session_rat') & session_key))).fetch1(
-                'experimenter', 'ratname', 'session_date')
-    sess_dir = root_dir / experimenter / ratname / f'{ratname}_{session_date.strftime("%Y_%m_%d")}'
-    return sess_dir.as_posix()
-    '''
+    return session_dir
 
 
-# ------------- Activate "ephys" schema -------------
-ephys_element.activate(ephys_schema_name, probe_schema_name, linking_module=__name__)
+# 4. Activate ephys schema -----------------------------------------------------
+ephys_element.activate(ephys_schema_name, 
+                       probe_schema_name, 
+                       linking_module=__name__)
 
-ephys_element.EphysRecording.key_source = Session.proj(ratname='session_rat') * ephys_element.ProbeInsertion
+ephys_element.EphysRecording.key_source = \
+    Session.proj(ratname='session_rat') * ephys_element.ProbeInsertion
 
 Session = Session.proj(...,ratname='session_rat')
