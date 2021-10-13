9693bdd400845f3e58664a15bb6c7b6fdb2ba8cb
Kabilar Gunalan <kabilar@vathes.com>
2021-10-13 15:18:48 -0400

    Fixes after populating datasets

--- notebooks/ephys_element/02-ephys-element-automate.py
+++ notebooks/ephys_element/02-ephys-element-automate.py
@@ -3,70 +3,60 @@ try_find_conf_file()
 
 import datajoint as dj
 
-from bl_pipeline import lab, subject, acquisition
-from bl_pipeline.ephys_element import ephys_element, probe_element
+from bl_pipeline.ephys_element import Session, ephys_element, probe_element
 from bl_pipeline.ingest import ephys_element_ingest
-from bl_pipeline.acquisition import AcquisitionSessions as Session
 
-params_ks = {
-    "fs": 30000,
-    "fshigh": 150,
-    "minfr_goodchannels": 0.1,
-    "Th": [10, 4],
-    "lam": 10,
-    "AUCsplit": 0.9,
-    "minFR": 0.02,
-    "momentum": [20, 400],
-    "sigmaMask": 30,
-    "ThPr": 8,
-    "spkTh": -6,
-    "reorder": 1,
-    "nskip": 25,
-    "GPU": 1,
-    "Nfilt": 1024,
-    "nfilt_factor": 4,
-    "ntbuff": 64,
-    "whiteningRange": 32,
-    "nSkipCov": 25,
-    "scaleproc": 200,
-    "nPCs": 3,
-    "useRAM": 0
-}
+# Required for ClusteringTask
+params_ks = {}
+# TODO insert parameters used for each session
 
 ephys_element.ClusteringParamSet.insert_new_params(
     'kilosort2', 0, 'Spike sorting using Kilosort2', params_ks)
 
-# TODO determine parameters used for each session
+session_query = Session
 
-for session_key in (Session & 'session_rat="A249"').fetch('KEY'):#session_rat, sessid
+populate_settings = {'display_progress': False, 
+                     'reserve_jobs': False, 
+                     'suppress_errors': False}
 
-    session_info  = (Session & session_key).fetch1()
-    print(session_info)
+for session_key in session_query.fetch('KEY'):
+    print(session_key)
+
     ephys_element_ingest.process_session(session_key)
 
-    ephys_element.EphysRecording.populate(session_key, display_progress=True)
+    ephys_element.EphysRecording.populate(session_key, **populate_settings)
 
-    clustering_relative_dir = (Session & session_key).fetch1('acquisition_post_rel_path')
+    session_info  = (Session & session_key).fetch1()
 
-    insertion_number = (ephys_element.ProbeInsertion & ('ratname="{0}"').format(session_info['session_rat'])).fetch1('insertion_number')
+    clustering_relative_dir = (Session & session_key).fetch1(
+                                                'acquisition_post_rel_path')
 
-    ephys_element.ClusteringTask.insert1(
-        dict(session_key, 
-            ratname=session_info['session_rat'], #TODO ratname
-            insertion_number=insertion_number, 
-            paramset_idx=0, 
-            clustering_output_dir=clustering_relative_dir
-            ), skip_duplicates=True)
-            #TODO paramset_idx and insertion number
+    insertion_query = ephys_element.ProbeInsertion &  ('ratname="{0}"').format(
+                                                        session_info['ratname'])
+    for insertion_number in insertion_query.fetch('insertion_number'):
 
-    ephys_element.Clustering.populate(session_key, display_progress=True)
+        # Insert new ClusteringTask for each ProbeInsertion
+        session_key_clustering_task = dict(session_key, 
+                                           ratname=session_info['ratname'],
+                                           insertion_number=insertion_number, 
+                                           paramset_idx=0)
+        # TODO update paramset_idx for the parameters used for each session 
 
-    session_key_clustering_task = (ephys_element.ClusteringTask & session_key).fetch1('KEY')
+        ephys_element.ClusteringTask.insert1(
+            dict(session_key_clustering_task, 
+                 clustering_output_dir=clustering_relative_dir,
+                 task_mode='load'), 
+                 skip_duplicates=True)
 
-    ephys_element.Curation().create1_from_clustering_task(session_key_clustering_task)
-    # Assumes curation was not performed on dataset
+        ephys_element.Clustering.populate(session_key, **populate_settings)
 
-    ephys_element.CuratedClustering.populate(session_key, display_progress=True)
-    ephys_element.LFP.populate(session_key, display_progress=True)
-    ephys_element.WaveformSet.populate(session_key, display_progress=True)
+        # Create a new curation entry.  In this case, since this script is 
+        # automated, no curation was assumed to be performed on the dataset
+        ephys_element.Curation().create1_from_clustering_task(
+                                                    session_key_clustering_task)
+        # TODO determine if any datasets were curated and update accordingly
+
+        ephys_element.CuratedClustering.populate(session_key, **populate_settings)
+        ephys_element.LFP.populate(session_key, **populate_settings)
+        ephys_element.WaveformSet.populate(session_key, **populate_settings)
 
