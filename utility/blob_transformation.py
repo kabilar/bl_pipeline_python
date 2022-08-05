95d7f8e0ce9445c5d6f9c385f2ea9b6868e1d515
Alvaro Luna <alvalunasan@gmail.com>
2022-08-05 10:26:02 -0400

    blob transformation function

---
+++ utility/blob_transformation.py
@@ -0,0 +1,42 @@
+
+import numpy as np
+import pandas as pd
+
+
+def mymblob_to_dict(np_array, as_int=True):
+    '''
+    Transform a numpy array to dictionary:
+    (numpy array are stored when saving Blobs in  MATLAB Datajoint, normally a dictionary will be the fit)
+    '''
+
+    # Transform numpy array to DF
+    out_dict = pd.DataFrame(np_array.flatten())
+
+    # Flatten each column and get the "real value of it"
+    out_dict = out_dict.applymap(lambda x: x.flatten())
+
+    #Get not empty columns to extract fist value of only those columns
+    s = out_dict.applymap(lambda x: x.shape[0])
+    not_empty_columns = s.loc[:, ~(s == 0).any()].columns.to_list()
+    out_dict = out_dict.apply(lambda x: x[0].flatten() if x.name in not_empty_columns else x)
+    
+    if not isinstance(out_dict, pd.DataFrame):
+        out_dict = out_dict.to_frame()
+        #Get columns that are "real" arrays not unique values disguised
+        s = out_dict.applymap(lambda x: x.size).T
+        real_array_columns = s.loc[:, (s > 1).any()].columns.to_list()
+        out_dict = out_dict.T
+        out_dict = out_dict.apply(lambda x: x[0] if x.name not in real_array_columns else x, axis=0)
+        
+    columns = out_dict.columns.copy()
+    out_dict = out_dict.squeeze()
+
+    #Transform numeric columns to int (normally for params)
+    if as_int:
+        for i in columns:
+            if (isinstance(out_dict[i],np.float64) and out_dict[i].is_integer()):
+                out_dict[i] = out_dict[i].astype('int')
+
+    out_dict = out_dict.to_dict()
+
+    return out_dict
