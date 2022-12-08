9e03900cc076f4378898bfe70a8a2046f1a285f5
Alvaro Luna <alvalunasan@gmail.com>
2022-12-07 23:25:06 -0600

    30 days for backup script

--- bl_pipeline/datajoint01_pipeline/process/cronjob_copyDB.sh
+++ bl_pipeline/datajoint01_pipeline/process/cronjob_copyDB.sh
@@ -5,11 +5,10 @@ echo $(pwd)
 source /home/u19prod@pu.win.princeton.edu/.bashrc
 source /home/u19prod@pu.win.princeton.edu/.bash_profile
 
-conda activate U19-pipeline_python_env
 cd "/home/u19prod@pu.win.princeton.edu/Datajoint_projs/bl_pipeline_python/"
 git pull
 
-num_days=3
+num_days=30
 
 conda activate bl_pipeline_python_env
 python scripts/transfer_data_oldDB.py $num_days
